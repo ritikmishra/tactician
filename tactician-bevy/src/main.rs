4b4e1b6e43283f4fd09a04f7765ce942ad847fa1
Ritik Mishra <ritik.mishra314@gmail.com>
2021-03-09 02:15:52 -0600

    feat(bevy): move physics sim stuff to separate plugin

--- tactician-bevy/src/main.rs
+++ tactician-bevy/src/main.rs
@@ -8,23 +8,19 @@ use std::ops::Add;
 
 mod bundles;
 mod components;
-
-/// Gravitational constant -- should probably be adjustable or something
-pub const G: f32 = 0.0000000006;
+mod physics;
+use physics::PhysicsPlugin;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(ShapePlugin)
+        .add_plugin(PhysicsPlugin)
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
         .add_startup_system(initialize_components.system())
         .add_system(enforce_size.system())
-        .add_system(move_objects.system())
-        .add_system(apply_gravity_from_planets_to_ships.system())
-        .add_system(apply_gravity_among_planets.system())
         .add_system(render_snailtrail.system())
-        .add_system(sprite_motion_system.system())
         .add_system(fps_counter.system())
         .run();
 }
@@ -142,7 +138,7 @@ fn render_snailtrail(
         trail.0.push(pos.0.convert());
 
         // TODO: remove magic number
-        if trail.0.len() > 5000 {
+        if trail.0.len() > 7000 {
             trail.0.remove(0);
         }
         commands.spawn(GeometryBuilder::build_as(
@@ -160,83 +156,6 @@ fn fps_counter(time: Res<Diagnostics>, mut texts: Query<&mut Text, With<FPSCount
             for mut text in texts.iter_mut() {
                 text.value = format!("{:.2}", fps_num);
             }
-        }
-    }
-}
-
-fn sprite_motion_system(mut physics_sprite: Query<(&mut Transform, &Position)>) {
-    for (mut sprite_pos, physics_pos) in physics_sprite.iter_mut() {
-        sprite_pos.translation = (physics_pos.0, 0.).into();
-    }
-}
-
-fn apply_gravity_from_planets_to_ships(
-    planets: Query<(&Position, &Mass), With<GravitySource>>,
-    mut ships: Query<(&Position, &mut Velocity), Without<GravitySource>>,
-    time: Res<Time>,
-) {
-    for (ship_pos, mut ship_vel) in ships.iter_mut() {
-        let aggregate_grav_accel = planets
-            .iter()
-            .map(|(Position(p_pos), Mass(p_mass))| {
-                // points from ship to planet
-                let pos_delta: Vec2 = *p_pos - ship_pos.0;
-                let dist2 = pos_delta.length_squared();
-
-                let accel_direction = pos_delta.normalize();
-
-                // don't multiply by ship mass - we want acceleration on ship (F = ma)
-                let accel_magnitude = G * p_mass / dist2;
-                return accel_direction * accel_magnitude;
-            })
-            .fold(Vec2::zero(), Vec2::add);
-
-        ship_vel.0 += aggregate_grav_accel * time.delta_seconds();
-    }
-}
-
-/// For all objects that have a position and a velocity, it moves the object
-/// according to the velocity and the time elapsed
-fn move_objects(mut objects: Query<(&mut Position, &Velocity)>, dt: Res<Time>) {
-    for (mut pos, Velocity(vel)) in objects.iter_mut() {
-        let pos_delta = dt.delta_seconds() * (*vel);
-        pos.0 += pos_delta;
-    }
-}
-
-fn apply_gravity_among_planets(
-    stars: Query<(Entity, &Position, &Mass), With<Star>>,
-    mut planets: Query<(Entity, &Position, &Mass, &mut Velocity), With<Planet>>,
-    time: Res<Time>,
-) {
-    // FIXME: uses aliased mutability :/
-    unsafe {
-        for mut planet in planets.iter_unsafe() {
-            let mut new_accel = Vec2::zero();
-            for gravity_source in planets
-                .iter_unsafe()
-                .map(|(a, b, c, _)| (a, b, c))
-                .chain(stars.iter())
-            {
-                if planet.0 != gravity_source.0 {
-                    let planet_position = (*planet.1).0;
-                    let gravity_source_pos = (*gravity_source.1).0;
-
-                    // TODO: this code is wet - same as the ship gravity impl, maybe we can combine the systems?
-                    // points from the planet to the gravity source
-                    let pos_delta: Vec2 = gravity_source_pos - planet_position;
-                    let dist2 = pos_delta.length_squared();
-
-                    let accel_direction = pos_delta.normalize();
-
-                    // don't multiply by ship mass - we want acceleration on ship (F = ma)
-                    let gravity_source_mass = (*gravity_source.2).0;
-                    let accel_magnitude = G * gravity_source_mass / dist2;
-
-                    new_accel += accel_direction * accel_magnitude;
-                }
-            }
-            planet.3 .0 += new_accel * time.delta_seconds();
         }
     }
 }
---
+++ tactician-bevy/src/physics.rs
@@ -0,0 +1,98 @@
+use bevy::prelude::*;
+
+use std::ops::Add;
+
+use crate::components::{GravitySource, Mass, Planet, Position, Star, Velocity};
+
+/// Gravitational constant -- should probably be adjustable or something
+pub const G: f32 = 0.0000000006;
+
+pub struct PhysicsPlugin;
+
+impl Plugin for PhysicsPlugin {
+    fn build(&self, app: &mut bevy::prelude::AppBuilder) {
+        app.add_system(apply_gravity_from_planets_to_ships.system())
+        .add_system(apply_gravity_among_planets.system())
+        .add_system(move_objects.system())
+        .add_system(move_sprite_to_physics_pos.system());
+    }
+}
+
+/// Makes the Position used in the physics simulation and the Transform used to render the sprite
+/// refer to the same physical location
+fn move_sprite_to_physics_pos(mut physics_sprite: Query<(&mut Transform, &Position)>) {
+    for (mut sprite_pos, physics_pos) in physics_sprite.iter_mut() {
+        sprite_pos.translation = (physics_pos.0, 0.).into();
+    }
+}
+
+fn apply_gravity_from_planets_to_ships(
+    planets: Query<(&Position, &Mass), With<GravitySource>>,
+    mut ships: Query<(&Position, &mut Velocity), Without<GravitySource>>,
+    time: Res<Time>,
+) {
+    for (ship_pos, mut ship_vel) in ships.iter_mut() {
+        let aggregate_grav_accel = planets
+            .iter()
+            .map(|(Position(p_pos), Mass(p_mass))| {
+                // points from ship to planet
+                let pos_delta: Vec2 = *p_pos - ship_pos.0;
+                let dist2 = pos_delta.length_squared();
+
+                let accel_direction = pos_delta.normalize();
+
+                // don't multiply by ship mass - we want acceleration on ship (F = ma)
+                let accel_magnitude = G * p_mass / dist2;
+                return accel_direction * accel_magnitude;
+            })
+            .fold(Vec2::zero(), Vec2::add);
+
+        ship_vel.0 += aggregate_grav_accel * time.delta_seconds();
+    }
+}
+
+fn move_objects(mut objects: Query<(&mut Position, &Velocity)>, dt: Res<Time>) {
+    for (mut pos, Velocity(vel)) in objects.iter_mut() {
+        let pos_delta = dt.delta_seconds() * (*vel);
+        pos.0 += pos_delta;
+    }
+}
+
+fn apply_gravity_among_planets(
+    stars: Query<(Entity, &Position, &Mass), With<Star>>,
+    mut planets: Query<(Entity, &Position, &Mass, &mut Velocity), With<Planet>>,
+    time: Res<Time>,
+) {
+    // FIXME: uses aliased mutability :/
+    unsafe {
+        for mut planet in planets.iter_unsafe() {
+            let mut new_accel = Vec2::zero();
+            for gravity_source in planets
+                .iter_unsafe()
+                .map(|(a, b, c, _)| (a, b, c))
+                .chain(stars.iter())
+            {
+                if planet.0 != gravity_source.0 {
+                    let planet_position = (*planet.1).0;
+                    let gravity_source_pos = (*gravity_source.1).0;
+
+                    // TODO: this code is wet - same as the ship gravity impl, maybe we can combine the systems?
+                    // points from the planet to the gravity source
+                    let pos_delta: Vec2 = gravity_source_pos - planet_position;
+                    let dist2 = pos_delta.length_squared();
+
+                    let accel_direction = pos_delta.normalize();
+
+                    // don't multiply by ship mass - we want acceleration on ship (F = ma)
+                    let gravity_source_mass = (*gravity_source.2).0;
+                    let accel_magnitude = G * gravity_source_mass / dist2;
+
+                    new_accel += accel_direction * accel_magnitude;
+                }
+            }
+            planet.3 .0 += new_accel * time.delta_seconds();
+        }
+    }
+}
+
+
